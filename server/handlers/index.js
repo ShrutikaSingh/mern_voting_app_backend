cefe05f70a8b25248248ecc7431ea93e10abea27
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-19 22:32:29 +0530

    created server with express &connected to mongodb database

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,12 +1,12 @@
 
 //next is ver imp to errors
-module.exports.notFound=(err,res,req,next)=>{
+module.exports.notFound=(res,req,next)=>{
  const err=new Error('not found');
  err.status=404;
  next(err);
 
 
-}
+};
 
 module.exports.errors = (err,res,req,next)=> {
     res.status(err.status || 500).json(//500 for any diff error
--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
 
+require('dotenv').config()//create private variables that u dont want to share on a website like github
 //create server
 const express = require('express');
+const cors=require('cors');  
+const bodyParser=require('body-parser');
 
-//import error handler
-const handle=require('./handlers')
-
-
-
+const handle=require('./handlers') //start listing at /
 const app=express();
-const port= 4000; //any no. above 1000,below 1000 is used by our computers
+//const port= process.env.PORT; //it will take the PORT variable from .env
+const port= 5000;
+app.use(cors());//initialise the middle wares that we installed
+app.use(bodyParser.json());//initialise the middle wares body-parser that we installed
 
-//start listing at /
-app.get('/',(req,res)=>res.json ({hello :'worlds'}));//json object
-
-
-
+app.get('/',(req,res)=>res.json ({hello :'worlds'}));//json object //start listing at /
 app.use(handle.notFound); //not found is coming from handler/index.js
 
 //its the final end point where the server lands before app crashes
