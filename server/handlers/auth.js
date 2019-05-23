3ca6fd56cf8cb2979ec427aa5e694373a63ed8b9
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-23 19:16:17 +0530

    create auth for handlers

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,19 +1,21 @@
-constdb = require('../models/mongo_index');//before anything we will be needing our database
+const db = require('../models');//before anything we will be needing our database
 //so we are using our db that will be asynchronous
 
 //we use diirectly exports instead of module.exports cauz in our handlers/index.js we are actually spreading it out using ...
 //so auth.js will be exports  and handlers/index.js will be module.exports
-module.exports.register = async(req,res,next) =>{ //async db , async always uses try catch //this way we will not need to do promises and callbacks
+exports.register = async(req,res,next) =>{ //async db , async always uses try catch //this way we will not need to do promises and callbacks
 try{
  const user= await db.User.create(req.body);//we need to create user i register since currently our database has no users & we r gonna create user using based on the body of request
- res,json(user);//now just send it back to the user this goona include the psswd //not to used during production
-}catch(err){
-    return next(err);
+  
+ res.json(user);//now just send it back to the user this goona include the psswd //not to used during production
+}
+catch(err){
+    next(err);
 }
 
 };
 
-module.exports.login= (req,res,next) => {
+exports.login= async (req,res,next) => {
     try{
 
     }catch(err){
