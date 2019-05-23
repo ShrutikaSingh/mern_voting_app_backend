cc6ec9a51de44cf6046ee7972697e3da9095c26f
ShrutikaSingh <shrutika051220@gmail.com>
2019-05-23 19:16:48 +0530

    create  handlers

--- server/handlers/index.js
+++ server/handlers/index.js
@@ -1,23 +1,24 @@
-module.exports=require('./auth');
+module.exports=require('./auth.js');
 //#002
-//module.exports = {  
-//    ...require('./auth') //... spread operater so as to spread out every individual handler inside of auth
-//};
-//go to handlers/auth.js #003
+module.exports = {
+    ...require('./auth'), //... spread operater so as to spread out every individual handler inside of auth
+};
+
 
 //next is ver imp to errors
 module.exports.notFound=(res,req,next)=>{
- const err=new Error('not found');
+ const err=new Error('not found,we are  in handlers/INDEX.js');
  err.status=404;
  next(err);
-
-
 };
 
+/*
 module.exports.errors = (err,res,req,next)=> {
-    res.status(err.status || 500).json(//500 for any diff error
+    res.status(err.status || 500).json(                    //500 for any diff error
         {
             err: err.message || 'Something went wrong'
          }
     );
+
 };
+*/
